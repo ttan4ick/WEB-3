ee418e1442cdd348f8d0a839caad50d546f8ff64
ttan4ick <tyulina.tanechka@yandex.ru>
2026-03-14 18:23:59 +0300

    task 13

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from data.jobs import Jobs
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from data import db_session
 from data.users import User
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+app.config["SECRET_KEY"] = "yandexlyceum_secret_key"
 
 
 @app.route("/")
@@ -16,10 +16,45 @@ def index():
     return render_template("index.html", jobs=jobs, names=names)
 
 
+@app.route("/register", methods=["POST", "GET"])
+def register():
+    if request.method == "GET":
+        return render_template(
+            "register.html", title="Регистрация", password_error=False
+        )
+    elif request.method == "POST":
+        email = request.form["email"]
+        password = request.form["password"]
+        retry_password = request.form["retry_password"]
+        surname = request.form["surname"]
+        name = request.form["name"]
+        age = request.form["age"]
+        position = request.form["position"]
+        speciality = request.form["speciality"]
+        address = request.form["address"]
+        if password == retry_password:
+            session = db_session.create_session()
+            user = User()
+            user.email = email
+            user.set_password(password)
+            user.surname = surname
+            user.name = name
+            user.age = int(age)
+            user.position = position
+            user.speciality = speciality
+            user.address = address
+            session.add(user)
+            session.commit()
+            return "Форма отправлена"
+        return render_template(
+            "register.html", title="Регистрация", password_error=True
+        )
+
+
 def main():
     db_session.global_init("db/mars_explorer.db")
-    app.run()
+    app.run(port=8080, host="127.0.0.1")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
